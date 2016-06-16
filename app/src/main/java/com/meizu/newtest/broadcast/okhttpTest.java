7323d60266afba54cd9c48bc6f268916f3dedd73
李斌辉 <libinhui@meizu.com>
2016-06-16 15:01:49 +0800

    添加自封装的整理常用方法的jar包

--- app/src/main/java/com/meizu/newtest/broadcast/okhttpTest.java
+++ app/src/main/java/com/meizu/newtest/broadcast/okhttpTest.java
@@ -5,42 +5,38 @@ import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
-import com.meizu.newtest.Util.okhttpUtils;
+
 import com.squareup.okhttp.OkHttpClient;
 
-import org.json.JSONException;
+import org.json.JSONObject;
 
-import java.io.IOException;
 import java.util.HashMap;
+import java.util.concurrent.ExecutionException;
 
+import static com.meizu.jarutil.okhttpUtils.getResponObj;
 
 /**
  * Created by libinhui on 2016/6/7.
  */
 public class okhttpTest extends BroadcastReceiver{
     OkHttpClient cl = new OkHttpClient();
-//    runThread rt = new runThread(cl,"http://172.17.132.13:8000/mzts/updatephonestatus/",);
-    runThread rt = new runThread();
-
     @Override
     public void onReceive(Context context, Intent intent) {
         if(intent.getAction().equals("test.bc.okhttptest")){
-            rt.start();
-        }
-    }
-
-    private class runThread extends Thread{
-        @Override
-        public void run() {
             HashMap hm = new HashMap();
+            HashMap hm2 = new HashMap();
             hm.put("sn","850ABLNFSWWK");
+            hm2.put("sn","850ABLNFSWWK");
             hm.put("phonestatus","1");
+            String url = "http://172.17.132.13:8000/mzts/updatephonestatus/";
+            String url2 = "http://172.17.132.13:8000/mzts/getphonestatus/";
             try {
-                String res = okhttpUtils.getPostResp(cl,"http://172.17.132.13:8000/mzts/updatephonestatus/",hm,"result");
-                Log.i("benlee",res);
-            } catch (IOException e) {
+                JSONObject res = getResponObj(cl,url,hm);
+                JSONObject res2 = getResponObj(cl,url2,hm2);
+                Log.i("benlee",res.toString()+"|"+res2.toString());
+            } catch (ExecutionException e) {
                 e.printStackTrace();
-            } catch (JSONException e) {
+            } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
